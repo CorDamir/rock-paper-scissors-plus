0ea82d030219c91f2f9275891bf844ea994e8ba3
Damir Ćorić <cordamir@gmail.com>
2024-09-10 00:29:21 +0000

    Add 'click' event listeners to game choices and a function to handle them

--- assets/jscript/game.js
+++ assets/jscript/game.js
@@ -1,3 +1,11 @@
-window.onload = function(){
-    
+function handleChoiceClick(){
+    let choiceId = this.getAttribute("id");
+    console.log(choiceId);
 }
+
+function initialSetup(){
+    let choices = document.getElementsByClassName("choice-container");
+    for (choice of choices) choice.addEventListener("click", handleChoiceClick);
+}
+
+window.onload = initialSetup();
