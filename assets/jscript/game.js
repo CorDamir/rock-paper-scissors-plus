d436f016dd904b942b7ba62871ed2167c3f6d0fe
Damir Ćorić <cordamir@gmail.com>
2024-09-10 00:41:50 +0000

    Add and test random choice function for computer

--- assets/jscript/game.js
+++ assets/jscript/game.js
@@ -1,6 +1,17 @@
+function chooseRandomly(){
+    switch(Math.round(Math.random()*4)){
+        case 0: return "rock";
+        case 1: return "scissors";
+        case 2: return "paper";
+        case 3: return "spock";
+        case 4: return "lizard";
+    }
+}
+
 function handleChoiceClick(){
-    let choiceId = this.getAttribute("id");
-    console.log(choiceId);
+    let playerChoice = this.getAttribute("id");
+    let computerChoice = chooseRandomly();
+    console.log(playerChoice + "VS" + computerChoice); 
 }
 
 function initialSetup(){
