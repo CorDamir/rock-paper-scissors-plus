021c22d6fb563354065ecf534b3d4d33c78ff409
Damir Ćorić <cordamir@gmail.com>
2024-09-11 22:10:52 +0000

    Add game starting functionality from settings

--- assets/jscript/settings.js
+++ assets/jscript/settings.js
@@ -1,11 +1,17 @@
+/** shows range value as text in settings */
 function handleRangeSetting(){
     document.getElementById("win-condition").innerText = this.value;
 }
 
+/** hides modal and calls game script */
 function startGame(){
-    alert("start pressed!");
+    for (let modal of document.getElementsByClassName("modal-container")) 
+        modal.style.display="none";
+    
+    initialSetup();
 }
 
+/** sets listeners for game start button and range input */ 
 function setSettings(){
     document.getElementById("start-button").addEventListener("click", startGame);
     document.getElementById("win-score").addEventListener("input", handleRangeSetting);
